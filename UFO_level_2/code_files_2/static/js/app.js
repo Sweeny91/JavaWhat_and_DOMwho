cd6f9d4e96a884da3f427587142c6841d010a468
Steven Sweeny <steven.sweeny@gmail.com>
2021-02-19 23:25:52 -0600

    copied over work from level 1

--- UFO_level_2/code_files_2/static/js/app.js
+++ UFO_level_2/code_files_2/static/js/app.js
@@ -1,11 +1,74 @@
-<<<<<<< HEAD
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-=======
-// from data.js
-var tableData = data;
+// Create variable to store body
+let tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
->>>>>>> 155554bd8c85279eb0082a633bcad2c9f7a6315d
+// print tableData for verification
+// console.log(tableData);
+
+// Function to Build Table
+function buildTable(table) {
+    // general for each function where placeholer "siting", refers to an entry in the given data set
+    table.forEach(function(siting) {
+
+        // console.log(siting);
+
+        // make js variable to append a row to the html tbody
+        let row = tbody.append("tr");
+
+        // Create key:value object and append each entry to to current row as such
+        Object.entries(siting).forEach(function([key, value]) {
+
+            // console.log(key, value);
+
+            let cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+buildTable(tableData);
+
+// Implement listener and initiate function to run when the button is clicked on the user side
+let button = d3.select("#filter-btn");
+let button2 = d3.select("#reset-btn");
+
+// Function to handle what to do when the button is clicked
+function handleClick() {
+
+    // prevent page from refreshing
+    d3.event.preventDefault();
+
+    // remove anything from, if not empty
+    tbody.html("");
+
+    // make variable to store value from "#datetime" input
+    let inputField = d3.select("#datetime");
+    let inputValue = inputField.property("value");
+
+    // console.log(inputValue);
+
+    // Create object to filter data by date
+    let filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+    if (filteredData.length === 0) {
+        buildtable(tableData)
+    } else buildtable(filteredData)
+}
+
+
+// Turn button click on
+button.on("click", handleClick);
+
+
+// Reset button configuration
+// function to handle reset
+function resetClick() {
+    tbody.html("");
+    buildtable(tableData)
+    document.getElementById("datetime").value = ""
+}
+
+// Initiate Reset
+button2.on("click", resetClick);
